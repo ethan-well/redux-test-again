f10a0b5949ed0c86de4d8f8c75c4ffe451d62897
魏巍 <wewin11235@163.com>
2018-12-27 10:26:24 +0800

    feat(learn-redux): filter todos

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -3,9 +3,22 @@ import { connect } from 'react-redux';
 import TodoList from '../components/TodoList';
 import { toggleTodo } from '../actions/index';
 
+const visibilityFilter = (todos, filter)  => {
+  switch (filter) {
+    case 'SHOW_ALL':
+      return todos
+    case 'SHOW_ACTIVE':
+      return todos.filter(todo => (!todo.completed))
+    case 'SHOW_COMPLETED':
+      return todos.filter(todo => (todo.completed))
+    default:
+      todso
+  }
+}
+
 const mapStateToProps = state => ({
-  todos: state.todos
-});
+  todos: visibilityFilter(state.todos, state.visibilityFilter)
+})
 
 const mapDispatchToProps = dispatch => ({
   toggleTodo: id => dispatch(toggleTodo(id))
